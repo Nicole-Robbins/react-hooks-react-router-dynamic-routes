cf57243b5af5fe2a8f7ff738d6c2a5e17ee840f1
Nicole Robbins <nicole.robbins@att.net>
2023-02-12 17:31:01 -0600

    commit tree 58c8b07648e1fc08563640405bef74ab823871e8

---
+++ src/components/MovieShow.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MovieShow() {
+    const params = useParams();
+    return (
+    <div>
+      <h3>{movies[params.movieId].title}</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
